cf2a8a20bcf0ad6f125958f53c1b0bc65b2b9b73
Frederik Bode <thorbensen@gmail.com>
2024-07-16 18:11:12 +0200

    feat(chart): added placeholder chart

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -27,6 +27,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import TopNavigation from "@/components/layout/top-navigation";
+import { PredictionChart } from "@/components/charts/prediction-chart";
 
 export default function Dashboard() {
   return (
@@ -88,6 +89,9 @@ export default function Dashboard() {
             </CardContent>
           </Card>
         </div>
+
+        <PredictionChart />
+
         <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
           <Card className="xl:col-span-2" x-chunk="dashboard-01-chunk-4">
             <CardHeader className="flex flex-row items-center">
